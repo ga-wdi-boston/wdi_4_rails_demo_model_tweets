99376d0bb7faec7c9c02f5c0212f75918d9c89d8
David Fisher <tibbon+github@gmail.com>
2013-07-09 10:42:32 -0400

    Added gender and favorite color migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+# Class name is singular
+# Tables names are plural (in the migrations, schema)
 class User < ActiveRecord::Base
   attr_accessible :email, :first_name, :last_name
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130709140214) do
+ActiveRecord::Schema.define(:version => 20130709144139) do
 
   create_table "books", :force => true do |t|
     t.string   "title"
@@ -35,8 +35,10 @@ ActiveRecord::Schema.define(:version => 20130709140214) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
+    t.string   "gender"
+    t.string   "favorite_color"
   end
 
 end
