67293b47d1fcba298e7d2892047f885de4c873a2
David Fisher <tibbon+github@gmail.com>
2013-07-09 11:28:13 -0400

    Added a tweets migration and model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,26 @@ class User < ActiveRecord::Base
   # Attributes are available because they are in the schema
   # not because we define them here
 
+  validates_presence_of :first_name, :last_name
+
+  # Intentionally commented out and invalid examples
+  # validates_acceptance_of :terms_of_service
+  # validates_confirmation_of :password, :repeated_password
+  # validates_format_of :phone_number, regex
+  # validates_numericality_of :price
+  # validates_presence_of :first_name
+  # validates_uniqueness_of :username
+
 # My custom getter
 def gender
   self[:gender] || 'Not specified'
 end
 
 
-
-
   attr_accessible :email, :first_name, :last_name
+  # This allows only the above to be assigned with mass parameters like
+  # User.new(params)
+  # To otherwise update them, you must use the assignment operator like
+  # User.first.is_admin = true
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130709145816) do
+ActiveRecord::Schema.define(:version => 20130709152553) do
 
   create_table "books", :force => true do |t|
     t.string   "title"
@@ -30,6 +30,15 @@ ActiveRecord::Schema.define(:version => 20130709145816) do
   end
 
   add_index "posts", ["user_id"], :name => "index_posts_on_user_id"
+
+  create_table "tweets", :force => true do |t|
+    t.string   "tweet"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "tweets", ["user_id"], :name => "index_tweets_on_user_id"
 
   create_table "users", :force => true do |t|
     t.string   "first_name"
